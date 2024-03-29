da22c2167df37b8cb89ecbf66c67ef5e1bd01ca6
Karthickraja <karthickraja.gm@gmail.com>
2024-03-29 13:11:05 +0530

    data types

---
+++ src/NonPrimitiveDataTypes.java
@@ -0,0 +1,25 @@
+
+public class NonPrimitiveDataTypes {
+	public static void main(String arg[]) {
+		
+		String str = "Karthickraja";
+		
+		System.out.println("str:" + str);
+		
+		String str1 = new String("Karthickraja 1");
+		
+		System.out.println("str:" + str1);
+		
+		int arr[];
+		
+		arr = new int[2];
+		
+		arr[0] = 1;
+		
+		
+		
+		
+		System.out.println("arr:" + arr[4]);
+		
+	}
+}
---
+++ src/PrimitiveDataTypes.java
@@ -0,0 +1,34 @@
+
+public class PrimitiveDataTypes {
+	public static void main(String args[]) {
+		char a = 'K';
+		
+		byte b = 127;
+		
+		short c = 456;
+		
+		long d = 4534353;
+		
+		float e = 4.1111111114324f;
+		
+		double f = 9.432432432432423d;
+		
+		boolean g = false;
+		
+		String k = "Karthick";
+		
+		char data[] = {'a', 'b', 'c'};
+	    String str = new String(data);
+		
+	    System.out.println(str);	
+		System.out.println(k);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		
+	}
+}
