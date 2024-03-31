4ab1f41a83f4316b77a2ea521be0fb4676599e01
Karthickraja <karthickraja.gm@gmail.com>
2024-03-31 08:56:25 +0530

    array study

---
+++ src/io/kar/study/ArraysType.java
@@ -0,0 +1,31 @@
+package io.kar.study;
+
+public class ArraysType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		byte[] arr;
+		
+		arr = new byte[4];
+		
+		arr[0] = 5;
+		arr[1] = 3;
+		arr[2] = 4;
+		arr[3] = (byte) 128;
+		
+		System.out.println("Element at index of 3: "+ arr[3]);
+		
+		String[] arr2;
+		
+		arr2 = new String[2];
+		
+		arr2[1] = "Karthickraja";
+		
+		System.out.println("Element at index of 1: "+ arr2[1]);
+		
+		
+
+	}
+
+}
